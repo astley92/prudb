651212fd90a9b4dd691b67922271210ffaa4cea8
Blake Astley <astley92@hotmail.com>
2021-06-20 01:02:09 +1000

    Move dev environment setup to correct file

--- environments/development.rb
+++ environments/development.rb
@@ -1,2 +1,18 @@
+def default_mailer_code
+  <<-CODE
+
+  # Setting mailer default url as required by devise.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+CODE
+end
+
 def set_development_environment
+  development_environment_file = Dir["config/environments/development.rb"].first
+
+  in_root {
+    insert_into_file(development_environment_file,
+      default_mailer_code,
+      after: "Rails.application.configure do"
+    )
+  }
 end
