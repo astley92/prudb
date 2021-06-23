9c0ad9f74b7f822edc6b5586e97f89a3d834ffbc
Blake Astley <astley92@hotmail.com>
2021-06-23 22:08:54 +1000

    Remove pg gem

--- template.rb
+++ template.rb
@@ -2,7 +2,6 @@ def apply_template!
   say("Using template to setup gems")
   gsub_file("Gemfile", /^gem\s+["']sqlite3["'].*$/,'')
   gem('devise')
-  gem('pg')
   gem_group(:development, :test) do
     gem("rspec-rails")
   end
